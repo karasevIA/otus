ff0a43fc531f9a9f1d080d554cf707dd9cd3be57
karasevIA <karasev.ia@phystech.edu>
2022-07-14 00:27:27 +0300

    add version

--- lesson_1/helloworld.cpp
+++ lesson_1/helloworld.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "version.h"
 
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    std::cout << int(PROJECT_VERSION_PATCH) << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
